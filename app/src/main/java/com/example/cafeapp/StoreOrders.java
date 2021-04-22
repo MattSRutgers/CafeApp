20deaf6d4cbeaeb56fb0b222dc901f2693fe1a3a
Matt S <ms3099@rutgers.edu>
2021-04-22 02:08:15 -0400

    added donut order removing

--- app/src/main/java/com/example/cafeapp/StoreOrders.java
+++ app/src/main/java/com/example/cafeapp/StoreOrders.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 
-public class StoreOrders extends AppCompatActivity {
+public class StoreOrders extends AppCompatActivity implements Customizable{
 
     private ArrayList<Order> storeOrderList = new ArrayList<>();
 
@@ -14,5 +14,20 @@ public class StoreOrders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_orders);
+    }
+
+    @Override
+    public boolean add(Object obj) {
+        if(obj != null) {
+            storeOrderList.add((Order)obj);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        storeOrderList.remove((Order) obj);
+        return true;
     }
 }
