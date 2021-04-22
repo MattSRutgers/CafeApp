dacd1d195d46f9b73aa2b452cb289661a99c1efe
Matt S <ms3099@rutgers.edu>
2021-04-22 01:49:08 -0400

    added donut order removing

--- app/src/main/java/com/example/cafeapp/OrderDetails.java
+++ app/src/main/java/com/example/cafeapp/OrderDetails.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,10 +14,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.cafeapp.DonutActivity.DONUT_ORDER_KEY;
+import static com.example.cafeapp.MainActivity.customerOrder;
 
 public class OrderDetails extends AppCompatActivity{
-    //private ArrayList<MenuItem> curOrderList;
+    private ArrayList<MenuItem> newOrderList = new ArrayList<>();
     //private  ArrayList<String> asString;
+    private int orderNumber = 1;
 
     //private ListView orderList;
 
@@ -36,13 +39,33 @@ public class OrderDetails extends AppCompatActivity{
         for(MenuItem item : curOrderList)
             asString.add(item.toString());
 
+        updateList(curOrderList);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, asString);
         orderList.setAdapter(arrayAdapter);
-        //ArrayAdapter <String> orderlist = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,customerOrder);
+        orderList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                asString.remove(i);
+                curOrderList.remove(i);
+                updateList(curOrderList);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+    private void updateList(ArrayList list){
+        newOrderList.addAll(list);
+
     }
 
+
     public void placeOrder (View view){
-        //super.approveOrder();
+        Order newOrder = new Order(orderNumber);
+        for(MenuItem item : newOrderList)
+            newOrder.add(item);
+        orderNumber ++;
+        customerOrder.add(newOrder);
+        newOrderList.clear();
 
     }
 }
--- app/src/main/java/com/example/cafeapp/StoreOrders.java
+++ app/src/main/java/com/example/cafeapp/StoreOrders.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class StoreOrders extends AppCompatActivity {
+
+    private ArrayList<Order> storeOrderList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
