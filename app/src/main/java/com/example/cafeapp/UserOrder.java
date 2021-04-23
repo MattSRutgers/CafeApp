3db2a3f23c4e64de09e21d87216e635466d66a31
Matt S <ms3099@rutgers.edu>
2021-04-22 22:47:43 -0400

    semi broken order totals

--- app/src/main/java/com/example/cafeapp/UserOrder.java
+++ app/src/main/java/com/example/cafeapp/UserOrder.java
@@ -8,18 +8,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import static com.example.cafeapp.MainActivity.customerOrder;
 import static com.example.cafeapp.MainActivity.storeOrder;
+import static com.example.cafeapp.MenuItem.doubleToDollar;
 
 /**
  * @Author Matthew Schilling and Gordon Miller
- * THe user order method keeps track of the users current order
+ * THe user order class keeps track of the users current order
  */
 public class UserOrder extends AppCompatActivity {
     private ArrayList<Order> passList = new ArrayList<>();
+    private static final double TAX_RATE = 0.06625;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +31,34 @@ public class UserOrder extends AppCompatActivity {
 
         Intent intent = getIntent();
         ArrayList<String> asString = new ArrayList<>();
+        double orderSubtotal = 0;
+        double tax = 0;
+        double total = 0;
 
         ListView orderList = (ListView) findViewById(R.id.userOrderListView);
         ArrayList<Order> userOrderList = (ArrayList<Order>) intent.getSerializableExtra("Test");
-        for(Order item : userOrderList)
+        for(Order item : userOrderList) {
             asString.add(item.toString());
+            double temp =  item.getTotal();
+            orderSubtotal += temp;
+        }
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, asString);
         orderList.setAdapter(arrayAdapter);
         updateList(userOrderList);
+
+        TextView subView = (TextView) findViewById(R.id.userSubValue);
+        TextView taxView = (TextView) findViewById(R.id.taxValue);
+        TextView totalView = (TextView) findViewById(R.id.totalValue);
+        String temp = doubleToDollar(orderSubtotal);
+        tax = orderSubtotal * TAX_RATE;
+        total = tax + orderSubtotal;
+        String totalTemp = doubleToDollar(total);
+        String taxTemp = doubleToDollar(tax);
+        subView.setText(temp);
+        taxView.setText(taxTemp);
+        totalView.setText(totalTemp);
+
         orderList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
